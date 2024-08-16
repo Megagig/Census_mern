e7053d78f112f12706546bf0cc58b320f53f303e
Megagig <megagigsoftwaresolutions.com>
2024-08-16 14:45:12 +0100

    Create the Mongoose object and connect to the database

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+//create the mongoose object
+const mongoose = require('mongoose');
+var mongoDB = 'mongoDB://127.0.0.1/censusdb';
 
-   
+//connect to the database
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//get the connection object
+var db = mongoose.connection;
